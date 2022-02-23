44cee25bddfa5adf2808c038dbc40bd836f4dc78
walter.velardo <walter.velardo@digitware.it>
2022-02-23 16:41:28 +0100

    ex 1.6 1.9

--- part1/unificate/src/App.js
+++ part1/unificate/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [point, setPoint]= useState(0)
+
+const goodVote = () => {
+  setGood(good + 1)
+  setAll(all + 1)
+  setPoint(point + 1)
+} 
+
+const neutralVote = () => {
+  setNeutral(neutral + 1)
+  setAll(all + 1)
+  setPoint(point + 0)
 }
 
-export default App;
+const baddVote = () => {
+  setBad(bad + 1)
+  setAll(all + 1)
+  setPoint(point + -1)
+}
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+
+      <button onClick={goodVote}>good</button>
+      <button onClick={neutralVote}>neutral</button>
+      <button onClick={baddVote}>bad</button>
+
+      <h2>Statistics</h2>
+      {all === 0 && <h3>no feedback given</h3> 
+      || <> 
+          <h3>good:{good}</h3>
+          <h3>neutral:{neutral}</h3>
+          <h3>bad:{bad}</h3>
+          <h3>all: {all} </h3>
+          <h3>average: {average} </h3>
+          <h3>point: {point / all} </h3>
+        </>}
+     
+    </div>
+  )
+}
+
+export default App
