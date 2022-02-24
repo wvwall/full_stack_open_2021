1a548c4d39ae28b633d0fa91a9dbb047a51546c6
Walter Velardo <waltervelardo@gmail.com>
2022-02-24 17:18:20 +0100

    2d

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,48 +1,70 @@
-import Note from './components/Note'
-import { useState } from 'react'
+import { useState, useEffect } from "react";
 
-const App = ({ notes }) => {
-  const [note, setNotes] = useState(notes)
-  const [newNote, setNewNote] = useState(
-    'a new note...'
-  )
-  const [showAll, setShowAll] = useState(true)
+import Note from "./components/Note";
+import noteService from "./service/notes";
 
-  const notesToShow = showAll
-  ? notes
-  : notes.filter(note => note.important === true)
+const App = () => {
+  useEffect(() => {
+    noteService.getAll().then((initialNotes) => {
+      setNotes(initialNotes);
+    });
+  }, []);
+
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState([]);
+
+  const toggleImportanceOf = (id) => {
+    const url = `http://localhost:3002/notes/${id}`;
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+
+    noteService.update(id, changedNote).then((returnedNote) => {
+      setNotes(
+        notes.map((note) => (note.id !== id ? note : returnedNote))
+      ).catch((error) => {
+        alert(`the note '${note.content}' was already deleted from server`);
+        setNotes(notes.filter((n) => n.id !== id));
+      });
+    });
+  };
 
   const addNote = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     const noteObject = {
       content: newNote,
-      date: new Date().toISOString(),
+      date: new Date(),
       important: Math.random() < 0.5,
-      id: notes.length + 1,
-    }
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
-  }
+    };
+    noteService.create(noteObject).then((returnedNote) => {
+      setNotes(notes.concat(returnedNote));
+      setNewNote("");
+    });
+  };
 
   const handleNoteChange = (event) => {
-    console.log(event.target.value)
-    setNewNote(event.target.value)
-  }
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
+
   return (
     <div>
       <h1>Notes</h1>
-      
+
       <ul>
-        {notesToShow.map(note => 
-          <Note key={note.id} note={note} />
-        )}
+        {notes.map((note) => (
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
+        ))}
       </ul>
       <form onSubmit={addNote}>
         <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
-      </form> 
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
